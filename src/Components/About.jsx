8b3b0efc5e335b7206d6f492be4e1817c7ab8c04
HASNA KKP <hasnanoushi66@gmail.com>
2024-07-03 20:13:56 +0530

    comm

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,6 +9,13 @@ function About() {
         <AnimatedSentence>
           <p className="aboutsentence" style={{marginBottom:0}}>
           "I am a dedicated front-end developer proficient in React and Next.js, driven to craft engaging web experiences with a focus on performance and usability."
+          </p><br/><br/>
+        </AnimatedSentence>
+        <AnimatedSentence>
+          <br/><br/>
+          <p className="aboutsentence" style={{marginBottom:0}}>
+          Mail:hasnanoushi66@gmail.com<br/>
+          Mob:+91 9746230887(India)
           </p>
         </AnimatedSentence>
       </div>
