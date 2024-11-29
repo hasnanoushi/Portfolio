b6d18f1e62c8e8b6e23caeea9dcbe35cea8851be
HASNA KKP <hasnanoushi66@gmail.com>
2024-11-29 15:14:52 +0530

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./About.css";
 import AnimatedSentence from "./AnimatedSentence";
-
+import { MdMail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
 function About() {
   return (
     <div className="about">
       <div>
         <AnimatedSentence>
-          <p className="aboutsentence" style={{marginBottom:0}}>
-          "I am a dedicated front-end developer proficient in React and Next.js, driven to craft engaging web experiences with a focus on performance and usability."
-          </p><br/><br/>
+          <p className="aboutsentence" style={{ marginBottom: 0 }}>
+            "I am a dedicated front-end developer proficient in React and
+            Next.js, driven to craft engaging web experiences with a focus on
+            performance and usability."
+          </p>
+          <br />
+          <br />
         </AnimatedSentence>
         <AnimatedSentence>
-          <br/><br/>
-          <p className="aboutsentence" style={{marginBottom:0}}>
-          Mail:hasnanoushi66@gmail.com<br/>
-          Mob:+91 9746230887(India)
+          <br />
+          <br />
+          <p className="aboutsentence" style={{ marginBottom: 0 }}>
+            <MdMail />
+            hasnanoushi66@gmail.com
+            <br />
+            <FaPhoneAlt />
+            +919746230887
           </p>
         </AnimatedSentence>
       </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,76 +1,61 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
-import image from '../Assets/hasn.jpeg';
-import TextSpan from './TextSpan';
-import { FaDev, FaLinkedin, FaTwitter } from "react-icons/fa";
+import image from "../Assets/hasn.jpeg";
+import TextSpan from "./TextSpan";
+import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
-
 
 function Home() {
-
-  const me = `I'm HASNA`.split('')
-  const dev = `FRONTEND DEVELOPER`.split('')
+  const me = `I'm HASNA`.split("");
+  const dev = `FRONTEND DEVELOPER`.split("");
 
   return (
-    <div className='home' >
-      
-      <div className='name-container'>
-          <div>
-            {me.map((letter, index) => {
-              return (
-                  <TextSpan key={index}>
-                    {letter ===" " ? "\u00A0":letter}
-                  </TextSpan>
-              
-              );
-            })}
-          </div>
+    <div className="home">
+      <div className="name-container">
+        <div>
+          {me.map((letter, index) => {
+            return (
+              <TextSpan key={index}>
+                {letter === " " ? "\u00A0" : letter}
+              </TextSpan>
+            );
+          })}
+        </div>
 
-          <div >
-            {dev.map((letter, index) => {
-              return (
-                  <TextSpan key={index}>
-                    {letter ===" " ? "\u00A0":letter}
-                  </TextSpan>
-              
-              );
-            })}
-          </div>
+        <div>
+          {dev.map((letter, index) => {
+            return (
+              <TextSpan key={index}>
+                {letter === " " ? "\u00A0" : letter}
+              </TextSpan>
+            );
+          })}
+        </div>
       </div>
 
-      <div className='imgDiv'>
-            <div className='imgFit'>
-              <img src={image} alt="Image" className='photo' />
-            </div>
-            <div>
-              <ul>
+      <div className="imgDiv">
+        <div className="imgFit">
+          <img src={image} alt="Image" className="photo" />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com/in/hasna-kkp-121b9b252/">
+                <FaLinkedin />
+              </a>
+            </li>
 
-                <li >
-                  <a href="https://www.linkedin.com/in/hasna-kkp-121b9b252/">
-                  <FaLinkedin />
-                  </a>
-                
-                </li>
-
-                <li>
-                  <a href="https://github.com/hasnanoushi">
-                  <FaGithub />
-                  </a>
-                
-                </li>
-                
-
-            
-         
-              </ul>
-            </div>
-            
+            <li>
+              <a href="https://github.com/hasnanoushi">
+                <FaGithub />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
